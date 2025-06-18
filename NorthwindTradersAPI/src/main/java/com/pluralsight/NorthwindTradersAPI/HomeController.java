cf9f8cbeabce911f18090dda504234866bbf7b21
Brian Miller <Bmiller995@gmail.com>
2025-06-18 15:04:02 -0500

    clean up

--- NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/HomeController.java
+++ NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/HomeController.java
@@ -1,4 +1,4 @@
-package com.pluralsight.NorthwindTradersAPI.controller;
+package com.pluralsight.NorthwindTradersAPI;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
