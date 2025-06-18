422b0ee83aacad99389c7f9e1abb92266cca2899
Brian Miller <Bmiller995@gmail.com>
2025-06-18 15:00:13 -0500

    Added Homecontoller

---
+++ NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/HomeController.java
@@ -0,0 +1,27 @@
+package com.pluralsight.NorthwindTradersAPI.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home(@RequestParam(required = false) String country) {
+        if (country != null) {
+            return "Hello " + country;
+        } else {
+            return "Hello World";
+        }
+    }
+
+    @GetMapping("/brian")
+    public String brianHome() {
+        return "Hello Brian";
+
+    }
+
+
+
+}
